6b6a13404609d737cd45ca1fd42f834b64c6ec2f
HASIBUZZAMAN <57929194+HasibuzzamanFUAS@users.noreply.github.com>
2025-04-24 15:37:01 +0200

    Update ip-sweeper.sh

--- ip-sweeper.sh
+++ ip-sweeper.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# usage
+# ./ip-sweeper.sh <ip address upto 3 octets e.g 192.186.1>
+
 for ip in $(seq 1 254); do
     #echo "Pinging $1.$ip"
     ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
